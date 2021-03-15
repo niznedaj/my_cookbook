b829489eeee98321df95026320de8e68f7a769fc
Jeff Denzin <jadenzin@gmail.com>
2021-03-15 09:55:55 -0500

    extract recipe data from the ui

--- recipes.py
+++ recipes.py
@@ -1,16 +1,25 @@
-import flask
+from flask import Flask, request, session, redirect, url_for, abort, render_template, flash
 import flask.views
 
 
 class ViewAll(flask.views.MethodView):
     def get(self):
+        if not session.get('username'):
+            abort(401)
         return flask.render_template('view_recipes.html')
 
 
 class Add(flask.views.MethodView):
     def get(self):
+        if not session.get('username'):
+            abort(401)
         return flask.render_template('add_recipe.html')
 
     def post(self):
-        flask.flash('Recipe added!  Enter another, or use the navigation above on the left.')
+        e = (request.form['recipe_name']
+             , request.form['ingredients']
+             , request.form['steps'])
+        #        return redirect(url_for('show_entries'))
+        print(e)
+        flash('%s added!  Enter another, or use the navigation above on the left.' % request.form['recipe_name'])
         return flask.render_template('add_recipe.html')
